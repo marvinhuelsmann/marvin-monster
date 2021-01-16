a13cbf8c7bc65f3d07d9efb195fa3b4541705569
Marvin Hülsmann <mrntv64@gmail.com>
2021-01-16 02:15:58 +0100

    init push

--- src/main.js
+++ src/main.js
@@ -1,11 +1,43 @@
 import '@babel/polyfill'
 import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/fontawesome'
+import {library} from '@fortawesome/fontawesome-svg-core'
+import {
+  faAngleDoubleDown,
+  faEnvelope,
+  faMapMarked,
+  faSignature,
+  faShare,
+  faCommentAlt,
+  faSave,
+  faPhone,
+  faShoppingBag,
+  faCheck,
+  faDownload,
+  faAngleDoubleRight,
+  faQuestionCircle,
+  faGamepad,
+  faAddressBook,
+  faRocket,
+  faThumbtack,
+  faBirthdayCake
+} from '@fortawesome/free-solid-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import './plugins/bootstrap-vue'
 import App from './App.vue'
+import router from './router'
+
+
+library.add(faBirthdayCake, faThumbtack, faAngleDoubleDown, faEnvelope, faMapMarked, faSignature, faShare, faCommentAlt, faSave, faPhone, faCheck, faShoppingBag, faDownload, faQuestionCircle, faGamepad, faAddressBook, faAngleDoubleRight, faRocket)
+
+
+Vue.component('fai', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
+
