814c1eadca6a9e987a007a69a55f68a28d95a8a0
Marvin Hülsmann <mrntv64@gmail.com>
2021-06-20 19:46:50 +0200

    adding game structur

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,5 @@
 <template>
-  <div id="app">
     <router-view/>
-  </div>
 </template>
 
-<style lang="scss">
-  @import "./assets/scss/app";
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
